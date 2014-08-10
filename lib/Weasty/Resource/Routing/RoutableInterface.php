c27eab73f69a74e653fb2d7209c1733344779ba4
ja1cap <ja1cap@me.com>
2014-08-11 01:14:01 +0300

    routable interface

---
+++ lib/Weasty/Resource/Routing/RoutableInterface.php
@@ -0,0 +1,15 @@
+<?php
+namespace Weasty\Resource\Routing;
+
+/**
+ * Interface RoutableInterface
+ * @package Weasty\Resource\Routing
+ */
+interface RoutableInterface {
+
+    /**
+     * @return array
+     */
+    public function getRouteParameters();
+
+} 
